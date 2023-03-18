e69b28e9899b0a68562e62d7d0c44785c302ee53
linmandla <milisakuse@gmail.com>
2023-03-18 10:38:47 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,11 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-
-
-
 /**
  * main - Prints if number is positive,zero or negative 
  * Return:Always (Success)
  *
  */
-
 int main(void)
 
 {
@@ -34,5 +28,4 @@ int main(void)
 	printf("%d is negative\n", n);
 	}
 	return (0);
-
 }	
