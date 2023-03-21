64646acb67f32d82f8f21ed83bf2e43be608ad51
linmandla <milisakuse@gmail.com>
2023-03-21 06:06:46 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	for (i = 'a' : i <= 'z' i i++)
+	     	putchar(i);
+	putchar("\m");
+	return (0)
+}		
